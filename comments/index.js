2a8d3033cb642bae9c1e9406b7d6c543a0246b03
AdhamHaisami <adham9633@gmail.com>
2023-06-02 07:49:49 +0300

    create an event for post and comment service

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json())
@@ -13,7 +14,7 @@ app.get('/post/:id/comments', (req, res) => {
     res.send(commentsByPostId[req.params.id] || [])
 })
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
     const commentId = randomBytes(4).toString('hex');
     const { content } = req.body;
     const postId = req.params.id;
@@ -22,6 +23,17 @@ app.post('/posts/:id/comments', (req, res) => {
 
     comments.push({
         id: commentId, content,
+    });
+
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commentId,
+            postId,
+            content
+        }
+    }).catch((err) => {
+        console.log(err.message);
     });
 
     commentsByPostId[postId] = comments;
