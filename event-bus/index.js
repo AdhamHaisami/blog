9c27fa4c649a3ef4d59a11dcfdd2811d691b72c0
AdhamHaisami <adham9633@gmail.com>
2023-06-02 06:31:03 +0300

    Implement a simple event-bus

---
+++ event-bus/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParse = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParse.json())
+
+
+app.post('/events', (req, res) => {
+    const event = req.body;
+
+    axios.post('http://localhost:4000/events', event).catch((err) => {
+        console.log(err.message);
+    })
+    axios.post('http://localhost:4001/events', event).catch((err) => {
+        console.log(err.message);
+    })
+    axios.post('http://localhost:4002/events', event).catch((err) => {
+        console.log(err.message);
+    })
+
+    res.send({ status: 'OK' });
+})
+
+const PORT = 4005;
+
+app.listen(PORT, () => {
+    console.log(`Event-bus server is listening on port ${PORT}`);
+})
