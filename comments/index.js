cbf0e159c5877c650bb0adb499b60ffa5248125f
AdhamHaisami <adham9633@gmail.com>
2023-06-02 08:05:08 +0300

    add event listener to the post and comment services

--- comments/index.js
+++ comments/index.js
@@ -1,53 +1,56 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { randomBytes } = require('crypto');
-const cors = require('cors');
-const axios = require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(cors());
 
 const commentsByPostId = {};
 
-app.get('/post/:id/comments', (req, res) => {
-    res.send(commentsByPostId[req.params.id] || [])
-})
-
-app.post('/posts/:id/comments', async (req, res) => {
-    const commentId = randomBytes(4).toString('hex');
-    const { content } = req.body;
-    const postId = req.params.id;
-
-    const comments = commentsByPostId[postId] || [];
-
-    comments.push({
-        id: commentId, content,
-    });
-
-    await axios.post('http://localhost:4005/events', {
-        type: 'CommentCreated',
-        data: {
-            id: commentId,
-            postId,
-            content
-        }
-    }).catch((err) => {
-        console.log(err.message);
-    });
-
-    commentsByPostId[postId] = comments;
-
-    res.status(201).send(comments);
-
-
-
+app.get("/post/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
 });
 
+app.post("/posts/:id/comments", async (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+  const postId = req.params.id;
 
+  const comments = commentsByPostId[postId] || [];
+
+  comments.push({
+    id: commentId,
+    content,
+  });
+
+  await axios
+    .post("http://localhost:4005/events", {
+      type: "CommentCreated",
+      data: {
+        id: commentId,
+        postId,
+        content,
+      },
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+
+  commentsByPostId[postId] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
+  res.send({});
+});
 
 const PORT = 4001;
 
 app.listen(PORT, () => {
-    console.log(`Comments server is lisitening on port ${PORT}`);
-})
+  console.log(`Comments server is lisitening on port ${PORT}`);
+});
--- posts/index.js
+++ posts/index.js
@@ -1,42 +1,48 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { randomBytes } = require('crypto');
-const cors = require('cors');
-const axios = require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(cors());
 const posts = {};
 
-app.get('/posts', (req, res) => {
-    res.send(posts)
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+app.post("/posts", async (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
 
-})
-app.post('/posts', async (req, res) => {
-    const id = randomBytes(4).toString('hex');
-    const { title } = req.body;
+  posts[id] = {
+    id,
+    title,
+  };
 
-    posts[id] = {
-        id, title
-    };
-
-    await axios.post('http://localhost:4005/events', {
-        type: 'PostCreated',
-        data: {
-            id, title
-        }
-
-    }).catch((err) => {
-        console.log(err.message);
+  await axios
+    .post("http://localhost:4005/events", {
+      type: "PostCreated",
+      data: {
+        id,
+        title,
+      },
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
 
-    res.status(201).send(posts[id]);
+  res.status(201).send(posts[id]);
+});
 
-})
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
+  res.send({});
+});
 
 const PORT = 4000;
 
 app.listen(PORT, () => {
-    console.log(`Posts server is lisitening on port ${PORT}`);
-})
+  console.log(`Posts server is lisitening on port ${PORT}`);
+});
