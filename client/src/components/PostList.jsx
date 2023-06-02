3db12fd7376efc52a410f1ae1155b7dd62a90fc4
AdhamHaisami <adham9633@gmail.com>
2023-06-02 09:44:46 +0300

    update react app to fetch data from query service instead of posts and comments services

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -1,42 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import CommentCreate from './CommentCreate';
-import CommetnList from './CommetnList';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import CommentCreate from "./CommentCreate";
+import CommetnList from "./CommetnList";
 
 function PostList() {
-    const [posts, setPosts] = useState({});
+  const [posts, setPosts] = useState({});
 
-    const fetchPosts = async () => {
-        const { data } = await axios.get('http://localhost:4000/posts');
-        console.log(data);
-        setPosts(data);
-    }
+  const fetchPosts = async () => {
+    // getting data from query service instead of post serrvice
+    // const { data } = await axios.get('http://localhost:4000/posts');
 
-    useEffect(() => {
-        fetchPosts();
-    }, []);
+    const { data } = await axios.get("http://localhost:4002/posts");
+    console.log(data);
+    setPosts(data);
+  };
 
-    const renderedPosts = Object.values(posts).map(post => {
-        return <div
-            className='card'
-            style={{ width: '30%', marginBottom: '20px' }}
-            key={post.id}
-        >
-            <div className='card-body'>
-                <h3>
-                    {post.title}
-                </h3>
-                <CommetnList postId={post.id} />
-                <CommentCreate postId={post.id} />
-            </div>
-        </div>
-    });
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
+  const renderedPosts = Object.values(posts).map((post) => {
     return (
-        <div className='d-flex flex-row flex-wrap justify-content-between'>
-            {renderedPosts}
+      <div
+        className="card"
+        style={{ width: "30%", marginBottom: "20px" }}
+        key={post.id}
+      >
+        <div className="card-body">
+          <h3>{post.title}</h3>
+          <CommetnList comments={post.comments} />
+          <CommentCreate postId={post.id} />
         </div>
-    )
+      </div>
+    );
+  });
+
+  return (
+    <div className="d-flex flex-row flex-wrap justify-content-between">
+      {renderedPosts}
+    </div>
+  );
 }
 
-export default PostList
+export default PostList;
